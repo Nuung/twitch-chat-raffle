cd0f1bf9846cea1c4f7a5696ab8e1de4c5c4f158
Nuung <qlgks1@naver.com>
2021-04-23 04:14:12 +0900

    update library

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,9 @@ mongoose.connect(`mongodb://${dbConfig.username}:${dbConfig.password}@${dbConfig
     console.log("DB Connection Error: " + err.message);
   });
 
-// server directory
-// const _dir = "/home/copfirst/amnotifyKR/amnotifyCrawler";
 
-// load DB schema -> 즉 collection이 on_off_check임
-// const On_off_check = require("../models/on_off_check");
+// load DB schema
+const TwitchRaffle = require("../models/twitch_raffle");
 
 // 넘어온 값이 빈값인지 체크합니다. -> !value 하면 생기는 논리적 오류를 제거하기 위해
 // 명시적으로 value == 사용 / [], {} 도 빈값으로 처리
